222410d19668220baa41875578c9e29ec707674d
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-08 11:44:54 +0530

    move to es modules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import { connect } from './config/database.js';
 
 const app = express();
 
-const TweetService  = require('./services/tweet-service');
+import service from './services/tweet-service.js';
 
 app.listen(3000,async () => {
     console.log('Server started');
     await connect();
-    let service = new TweetService();
-    const tweet = await service.create({
-        content: 'my #working twitter'
-    });
-    console.log(tweet);
+    let ser = new service();
+    await ser.create({content : 'Done with #refactor'});
 }); 
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+import mongoose  from "mongoose";
 
-const commentSchema = new mongoose.Schema({
+ const commentSchema = new mongoose.Schema({
     comments :[{
         content: {
             type: String,
@@ -11,4 +11,4 @@ const commentSchema = new mongoose.Schema({
 
 const Comment = mongoose.model('Comment',commentSchema);
 
-module.exports = Comment;
+export default Comment;
