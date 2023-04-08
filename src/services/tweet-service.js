d0a7050a3b94bef58de88fbfc5a4bf81a08426ed
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-08 19:13:32 +0530

    Added Like model and basic user model

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -9,8 +9,8 @@ class TweetService {
      async create(data) {
         const content = data.content;
         const tags = content.match(/#[a-zA-Z0-9_]+/g)
-                     .map((tag) => tag.substring(1))
-                     .map(tag => tag.toLowerCase());
+                     .map((tag) => tag.substring(1).toLowerCase());
+                     
         const tweet = await this.tweetRepository.create(data);
         //we need to filter those hashtags which are not present in array of array
         let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
