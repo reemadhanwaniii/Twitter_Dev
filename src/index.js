444fbc363a116ad954d14b9b838d10ac2bc31d72
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-08 10:56:45 +0530

    Added hashtag repository

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const connect = require('./config/database');
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+
 const app = express();
+
+const { HashtagRepository} = require('./repository/index');
 
 app.listen(3000,async () => {
     console.log('Server started');
     await connect();
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.getAll(0,4);
-    // console.log(tweet[0]._id);
-    // console.log(tweet[0].id);
-    console.log(tweet[0].contentWithEmail);
+    // let repo = new HashtagRepository();
+    // await repo.bulkCreate([
+    //     {
+    //         title: 'Trend',
+    //         tweets: []
+    //     },
+    //     {
+    //         title: 'Excited',
+    //         tweets: []
+    //     },
+    //     {
+    //         title: 'python',
+    //         tweets: []
+    //     },
+    //     {
+    //         title: 'fun',
+    //         tweets: []
+    //     },
+    //     {
+    //         title: 'Career',
+    //         tweets: []
+    //     }
+    // ]);
+
+    // let repo = new HashtagRepository();
+    // const tweets = await repo.findByName(['trend','Career','python','tweets','reema']);
+    // console.log(tweets);
 }); 
