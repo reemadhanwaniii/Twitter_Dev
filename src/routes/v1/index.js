ca396a08e6b5cbc6d961793e45bd39134b75ea49
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-10 18:46:50 +0530

    Added signup with encrypted password

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { createTweet,getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
+import { signup } from '../../controllers/auth-controller.js';
 
 const router = express.Router();
 
 router.post('/tweet',createTweet);
 router.post('/like/toggle',toggleLike);
 router.post('/comment',createComment);
+router.post('/signup',signup);
 router.get('/tweet/:id',getTweet);
 
 export default router;
